7fd3041932ef976cd868f59857eaa24f59b9fb70
Ivan-Asen Chakarov <vankata9907@gmail.com>
2018-02-02 00:09:43 +0200

    Added basic functionality for buying tickets

---
+++ contracts/SmartTickets.sol
@@ -0,0 +1,67 @@
+pragma solidity ^0.4.18;
+
+import "../node_modules/zeppelin-solidity/contracts/lifecycle/Pausable.sol";
+
+contract SmartTicketsCore is Pausable {    
+
+    enum EventStatus {
+        PASSED, NOT_PASSED, CANCELLED
+    }
+
+    struct TicketStore {
+        uint initialSupply;
+        uint currentSupply;
+        uint price;
+        string name;
+        string description;
+        address creator;
+        string metaDescription;
+        EventStatus status;
+    }
+
+    mapping (address => uint8) admins;
+
+    TicketStore[] ticketStores;    
+    mapping (uint => address) ticketStoreIdToCreator;
+    mapping (uint => address) ticketToOwner;
+    mapping (address => mapping (uint => uint)) ownerToTicketId;
+    mapping (address => uint) ticketBalanceOf;
+
+    modifier onlyAdmin() {
+        require(admins[msg.sender] == 1);
+        _;
+    }
+
+    function SmartTicketsCore() public {
+        admins[msg.sender] = 1;
+    }
+
+    function buyTicket(uint _ticketId, address _ticketFor) external payable {
+        TicketStore storage ticketStore = ticketStores[_ticketId];
+        // Check if ticketStore exists
+        require(ticketStore.creator != address(0));
+        require(ticketStore.active);
+        require(msg.value == ticketStore.price);
+        require(ticketStore.currentSupply > 0);
+
+        if (_ticketFor == address(0)) {
+            ownerToTicketId[msg.sender][_ticketId]++;
+            ticketStore.creator.send(ticketStore.price);
+            ticketStore.currentSupply--;
+        }
+    }
+
+    function createTicket(
+        uint _initialSupply,
+        uint _price,
+        string _metaDescription,
+        uint8 _status,
+        address _creator) public onlyAdmin returns(bool) {
+        return true;
+    }
+
+    function activateTicket() onlyAdmin {}
+
+    function deactivateTicket() onlyAdmin {}
+    
+}
