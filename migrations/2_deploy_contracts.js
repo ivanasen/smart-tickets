17004545fbcfe840e2241374c8a42be7c3eeebcc
Ivan-Asen Chakarov <vankata9907@gmail.com>
2018-03-03 10:29:39 +0200

    updated open-zeppelin

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
-
+// var ConvertLib = artifacts.require("./ConvertLib.sol");
+var SmartTicketsCore = artifacts.require("./SmartTicketsCore.sol");
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);  
+  // deployer.deploy(ConvertLib);
+  deployer.deploy(SmartTicketsCore);
 };
